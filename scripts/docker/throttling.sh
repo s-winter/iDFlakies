cae43c45458294696067c375c1a355e409f4af47
Stefan Winter <sw@cs.tu-darmstadt.de>
2019-11-21 22:58:17 +0100

    changed io throttling format to work with docker 17.05

--- scripts/docker/throttling.sh
+++ scripts/docker/throttling.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # CPU throttling: CPU(s) used and CPU time
-export THROTTLING_CPUSET='--cpuset-cpus=0'
-export THROTTLING_CPUS='--cpus=.01'
+export THROTTLING_CPUSET='--cpuset-cpus=0,1'
+export THROTTLING_CPUS='--cpus=.1'
 
 # Memory throttling: Main memory available, swap space available (-1 means unlimited, 0 means no swapping, else must be larger than memory), oom killer disabling
 export THROTTLING_MEM='--memory=500m'
@@ -10,12 +10,12 @@ export THROTTLING_SWAP='--memory-swap=-1'
 export THROTTLING_OOM='--oom-kill-disable'
 
 # blkio ops throttling (useful for workloads with small files)
-export THROTTLING_READ_IOPS='--device-read-iops /dev/sda:10'
-export THROTTLING_WRITE_IOPS='--device-write-iops /dev/sda:10'
+export THROTTLING_READ_IOPS='--device-read-iops=/dev/sda:10'
+export THROTTLING_WRITE_IOPS='--device-write-iops=/dev/sda:10'
 
 # blkio throughput throttling (useful for workloads with large files)
-export THROTTLING_READ_BPS='--device-read-bps /dev/sda:1k'
-export THROTTLING_WRITE_BPS='--device-write-bps /dev/sda:1k'
+export THROTTLING_READ_BPS='--device-read-bps=/dev/sda:1k'
+export THROTTLING_WRITE_BPS='--device-write-bps=/dev/sda:1k'
 
 # network throttling
 export THROTTLING_NIC='ON'
