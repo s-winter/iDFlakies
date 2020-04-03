9301431163f162e37a1a11d6361b52816e1afc2a
Stefan Winter <sw@stefan-winter.net>
2020-04-03 17:39:00 +0000

    changed throttling and multiprocessing to do 'old-school' iDFlakies for calibration

--- scripts/docker/run-pool.sh
+++ scripts/docker/run-pool.sh
@@ -54,42 +54,43 @@ echo $projectCSVs | xargs -P"$PROCESS_NUM" -I{} bash run-build-pool.sh {}
 
 # CPUCOUNT is an integer defined in throttling-system.sh
 # to make sure we don't accidentally run more parallel
-cpuGroups=$(($physProcs / $CPUCOUNT))
-declare -a CPUs
-for d in {1..$cpuGroups}; do
-    mkdir -p ${csvDir}/eGroup$d
-    CPUs[$d]=$(seq -s',' $((($d-1) * $CPUCOUNT)) $(($d * $CPUCOUNT - 1)))
-done
-i=0
+# cpuGroups=$(($physProcs / $CPUCOUNT))
+# declare -a CPUs
+# for d in {1..$cpuGroups}; do
+#     mkdir -p ${csvDir}/eGroup$d
+#     CPUs[$d]=$(seq -s',' $((($d-1) * $CPUCOUNT)) $(($d * $CPUCOUNT - 1)))
+# done
+# i=0
 # CPUFRAC is a float defined in throttling-system.sh that specifies what fraction of CPU time the container gets
 # CPU time is *per core*, which is why we scale according to CPU count:
-CPUFRAC=$((CPUFRAC * CPUCOUNT))
-for p in $(echo $projectCSVs); do
-    index=$(($i % $PROCESS_NUM + 1))
-    dirPath=${csvDir}/eGroup$index
-    cp $p $dirPath
-    throttlingFilePath=$dirPath/throttling.sh
-    cp throttling-template.sh $throttlingFilePath
-    sed -i "s:\$CPUSET:$CPUs[$index]:" $throttlingFilePath
-    sed -i "s:\$CPUFRAC:$CPUFRAC:" $throttlingFilePath
-    ((++i))
-done
+# CPUFRAC=$((CPUFRAC * CPUCOUNT))
+# for p in $(echo $projectCSVs); do
+#     index=$(($i % $PROCESS_NUM + 1))
+#     dirPath=${csvDir}/eGroup$index
+#     cp $p $dirPath
+#     throttlingFilePath=$dirPath/throttling.sh
+#     cp throttling-template.sh $throttlingFilePath
+#     sed -i "s:\$CPUSET:$CPUs[$index]:" $throttlingFilePath
+#     sed -i "s:\$CPUFRAC:$CPUFRAC:" $throttlingFilePath
+#     ((++i))
+# done
 
 # we divide 1 (for the compute time of 1 cpuGroup) by *twice* the CPUFRAC to keep CPUs idle at least 50% of time if CPUFRAC <= 50%
 # the reason for limiting the load of parallel instances on the same CPU group is to limit side effects from our parallelization
 # we cut off everything after the decimal dot to get the number of instances we can run on each CPU group
-if (($CPUFRAC > .5));
-then
+# if (($CPUFRAC > .5));
+# then
     perGroupInstances=1
-else
-    perGroupInstances=${$((1/(($CPUFRAC * 2))))%.*}
-fi
+# else
+#     perGroupInstances=${$((1/(($CPUFRAC * 2))))%.*}
+# fi
 
 if [ "$THROTTLING_NIC" = 'ON' ]
 then
     for i in $(sudo ifconfig |grep '.*: ' |cut -d':' -f1); do sudo wondershaper $i ${THROTTLING_NIC_DOWN} ${THROTTLING_NIC_UP}; done
 fi
-find $csvDir -maxdepth 1 -type d -name "eGroup*" | xargs -P"$cpuGroups" -I{} bash run-project-pool-wrapper.sh {} "$2" "$3" "$4" "$perGroupInstances"
+
+find $csvDir -maxdepth 1 -type f -name "*.csv" | xargs -P"$PROCESS_NUM" -I{} bash run-project-pool.sh {} "$2" "$3" "$4"
 
 if [ "$THROTTLING_NIC" = 'ON' ]
 then
--- scripts/docker/throttling-system.sh
+++ scripts/docker/throttling-system.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # network throttling
-export THROTTLING_NIC='OFF'
-export THROTTLING_NIC_DOWN=53
-export THROTTLING_NIC_UP=33
+# export THROTTLING_NIC='OFF'
+# export THROTTLING_NIC_DOWN=53
+# export THROTTLING_NIC_UP=33
 
-# IOPS
-export IOPSR=100
-export IOPSW=100
+# # IOPS
+# export IOPSR=100
+# export IOPSW=100
 
-# IO BPS
-export BPSR=100kb
-export BPSW=100kb
+# # IO BPS
+# export BPSR=100kb
+# export BPSW=100kb
 
-# memory
-export MEM=500m
+# # memory
+# export MEM=500m
 
-# CPU
-export CPUFRAC=.3
-export CPUCOUNT=1
+# # CPU
+# export CPUFRAC=.3
+# export CPUCOUNT=1
