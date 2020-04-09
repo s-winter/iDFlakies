fd8504bbe22a039e118917054fd9cdc066417883
Stefan Winter <sw@cs.tu-darmstadt.de>
2020-04-09 12:47:44 +0200

    adjusted throttling config for full throttling

--- scripts/docker/throttling-system.sh
+++ scripts/docker/throttling-system.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # network throttling
-export THROTTLING_NIC='OFF'
+export THROTTLING_NIC='ON'
 export THROTTLING_NIC_DOWN=53
 export THROTTLING_NIC_UP=33
 
 # IOPS
-export IOPSR=100
-export IOPSW=100
+export IOPSR=1
+export IOPSW=1
 
 # IO BPS
-export BPSR=100kb
-export BPSW=100kb
+export BPSR=1kb
+export BPSW=1kb
 
 # memory
-export MEM=500m
+export MEM=128m
 
 # CPU
-export CPUFRAC=.3
+export CPUFRAC=.1
 export CPUCOUNT=1
