bd8e8989d45d7cb2ccede5c075ed0433ad7a851d
Stefan Winter <sw@cs.tu-darmstadt.de>
2020-04-19 14:59:58 +0200

    adjusted i/o throttling config to resemble a sata 5400 rpm drive

--- scripts/docker/throttling-system.sh
+++ scripts/docker/throttling-system.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # network throttling
-export THROTTLING_NIC='OFF'
+export THROTTLING_NIC='ON'
 export THROTTLING_NIC_DOWN=53
 export THROTTLING_NIC_UP=33
 
 # IOPS
-export IOPSR=100
-export IOPSW=100
+export IOPSR=55
+export IOPSW=55
 
 # IO BPS
-export BPSR=100kb
-export BPSW=100kb
+export BPSR=3500kb
+export BPSW=3500kb
 
 # memory
-export MEM=500m
+export MEM=128m
 
 # CPU
-export CPUFRAC=.3
+export CPUFRAC=.1
 export CPUCOUNT=1
