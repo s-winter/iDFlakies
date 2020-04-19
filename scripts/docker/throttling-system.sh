9eadab3568446353ed4af2a495ade85f9cd6603e
Stefan Winter <sw@cs.tu-darmstadt.de>
2020-04-19 14:48:51 +0200

    thtrottling config I worked with earlier and which is known to produce results

--- scripts/docker/throttling-system.sh
+++ scripts/docker/throttling-system.sh
@@ -6,16 +6,16 @@ export THROTTLING_NIC_DOWN=53
 export THROTTLING_NIC_UP=33
 
 # IOPS
-export IOPSR=1
-export IOPSW=1
+export IOPSR=10
+export IOPSW=10
 
 # IO BPS
-export BPSR=1kb
-export BPSW=1kb
+export BPSR=10kb
+export BPSW=10kb
 
 # memory
-export MEM=128m
+export MEM=512m
 
 # CPU
 export CPUFRAC=.1
-export CPUCOUNT=1
+export CPUCOUNT=2
