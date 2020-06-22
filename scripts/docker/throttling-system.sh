5d4e605ff7e0fb693f3b74a3f9f1d49054dd855e
Stefan Winter <sw@cs.tu-darmstadt.de>
2020-06-22 09:42:36 +0200

    adjusted throttling configuration to combine previously tested configs

--- scripts/docker/throttling-system.sh
+++ scripts/docker/throttling-system.sh
@@ -5,17 +5,17 @@ export THROTTLING_NIC='ON'
 export THROTTLING_NIC_DOWN=53
 export THROTTLING_NIC_UP=33
 
-# # IOPS
-export IOPSR=100
-export IOPSW=100
+# IOPS
+export IOPSR=10
+export IOPSW=10
 
-# # IO BPS
-export BPSR=100kb
-export BPSW=100kb
+# IO BPS
+export BPSR=10kb
+export BPSW=10kb
 
-# # memory
-export MEM=500m
+# memory
+export MEM=128m
 
-# # CPU
-export CPUFRAC=.3
+# CPU
+export CPUFRAC=.1
 export CPUCOUNT=1
