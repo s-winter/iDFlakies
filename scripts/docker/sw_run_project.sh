0f2e06c48044802fb2dcd60ed4cce781701baf31
Stefan Winter <sw@stefan-winter.net>
2020-06-22 16:28:31 +0000

    merged the run logic from run_mvn_surefire.sh and sw_run_project.sh

--- scripts/docker/sw_run_project.sh
+++ scripts/docker/sw_run_project.sh
@@ -11,48 +11,59 @@ if [[ $1 == "" ]] || [[ $2 == "" ]] || [[ $3 == "" ]] || [[ $4 == "" ]]; then
     echo "arg1 - GitHub SLUG"
     echo "arg2 - Number of rounds"
     echo "arg3 - Timeout in seconds"
-    echo "arg4 - docker image name"
+    echo "arg4 - Test name (Optional)"
+    echo "arg5 - Round index to start from"
+    echo "arg6 - runId" 
     exit
 fi
 
 slug=$1
-class=$2
-rounds=$3
-timeout=$4
-image=$5
+rounds=$2
+timeout=$3
+fullTestName=$4
+roundsStart=$5
+runId=$6
+
+[[ -z $roundsStart ]] && roundsStart=1
+
+echo "Slug: ${slug}"
+echo "Rounds: ${rounds}"
+echo "Timeout: ${timeout}"
+echo "fullTestName: ${fullTestName}"
+echo "roundsStart: ${roundsStart}"
+echo "runId: ${runId}"
 
 modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
 
 # the following named pipes are used for synchronization with the host
 # right before the script ends, SCRIPTEND is signaled by the script running inside the container
 # then the host reads cgroup accounting data from sysfs and signals DATAREAD to indicate that the script can finish
-[ -w /Scratch/SCRIPTEND_${image} ] || { echo "SCRIPTEND named pipe for host synchronization does not exist or is not writable"; exit 1; }
-[ -r /Scratch/DATAREAD_${image} ] || { echo "DATAREAD named pipe for host synchronization does not exist or is not writable"; exit 1; }
+[ -w /Scratch/SCRIPTEND_${runId} ] || { echo "SCRIPTEND named pipe for host synchronization does not exist or is not writable"; exit 1; }
+[ -r /Scratch/DATAREAD_${runId} ] || { echo "DATAREAD named pipe for host synchronization does not exist or is not writable"; exit 1; }
 
 # Incorporate tooling into the project, using Java XML parsing
 # cd "/home/$SCRIPT_USERNAME/${slug}"
 # /home/$SCRIPT_USERNAME/$TOOL_REPO/scripts/docker/pom-modify/modify-project.sh . 1.0.1-SNAPSHOT
 
+export PATH=/home/$SCRIPT_USERNAME/apache-maven/bin:$PATH
+
+cd /home/$SCRIPT_USERNAME/
+
+cd /home/$SCRIPT_USERNAME/${slug}
+
 # Set global mvn options for skipping things
 MVNOPTIONS="-Ddependency-check.skip=true -Dgpg.skip=true -DfailIfNoTests=false -Dskip.installnodenpm -Dskip.npm -Dskip.yarn -Dlicense.skip -Dcheckstyle.skip -Drat.skip -Denforcer.skip -Danimal.sniffer.skip -Dmaven.javadoc.skip -Dfindbugs.skip -Dwarbucks.skip -Dmodernizer.skip -Dimpsort.skip -Dmdep.analyze.skip -Dpgpverify.skip -Dxml.skip"
 
-# Optional timeout... In practice our tools really shouldn't need 1hr to parse a project's surefire reports.
-# timeout 1h /home/$SCRIPT_USERNAME/apache-maven/bin/mvn testrunner:testplugin ${MVNOPTIONS} -Dtestplugin.className=edu.illinois.cs.dt.tools.utility.ModuleTestTimePlugin -fn -B -e |& tee module_test_time.log
+if [[ $fullTestName == "org.apache.hadoop.hbase.regionserver.TestSyncTimeRangeTracker" || $fullTestName == "org.apache.hadoop.hbase.snapshot.TestMobRestoreSnapshotHelper" ]]; then
+    formatTest="$(echo $fullTestName | rev | cut -d. -f2 | rev)"
+    class="$(echo $fullTestName | rev | cut -d. -f2 | rev)"
+else
+    formatTest="$(echo $fullTestName | rev | cut -d. -f2 | rev)#$(echo $fullTestName | rev | cut -d. -f1 | rev )"
+    class="$(echo $fullTestName | rev | cut -d. -f2 | rev)"
+fi
 
-# # Run the plugin, reversing the original order (reverse class and methods)
-# echo "*******************REED************************"
-# echo "Running testplugin for reversing the original order"
-# date
-
-# timeout 4000s /home/$SCRIPT_USERNAME/apache-maven/bin/mvn testrunner:testplugin ${MVNOPTIONS} -Ddetector.timeout=4000 -Ddt.randomize.rounds=${rounds} -Ddetector.detector_type=reverse -fn -B -e |& tee reverse_original.log
-
-
-# # Run the plugin, reversing the original order (reverse class)
-# echo "*******************REED************************"
-# echo "Running testplugin for reversing the class order"
-# date
-
-# timeout 4000s /home/$SCRIPT_USERNAME/apache-maven/bin/mvn testrunner:testplugin ${MVNOPTIONS} -Ddetector.timeout=4000 -Ddt.randomize.rounds=${rounds} -Ddetector.detector_type=reverse-class -fn -B -e |& tee reverse_class.log
+echo "formatTest: $formatTest"
+echo "class: $class"
 
 classloc=$(find -name $class.java)
 if [[ -z $classloc ]]; then
@@ -60,6 +71,14 @@ if [[ -z $classloc ]]; then
     exit 100
 fi
 
+classcount=$(find -name $class.java | wc -l)
+if [[ "$classcount" != "1" ]]; then
+    classloc=$(find -name $class.java | head -n 1)
+    echo "Multiple test class found. Unsure which one to use. Choosing: $classloc. Other ones are:"
+    find -name $class.java
+fi
+
+module=$classloc
 while [[ "$module" != "." && "$module" != "" ]]; do
     module=$(echo $module | rev | cut -d'/' -f2- | rev)
     echo "Checking for pom at: $module"
@@ -67,70 +86,31 @@ while [[ "$module" != "." && "$module" != "" ]]; do
 	break;
     fi
 done
+echo "Location of module: $module"
+
+RESULTSDIR=/Scratch/output/$modifiedslug_$module
+mkdir -p ${RESULTSDIR}
 
 # Run the plugin, original order
 echo "*******************REED************************"
 echo "Running mvn test on $module"
 date
 
-mkdir /Scratch/results-$modifiedslug-$module
-
-cd $slug
-
-# remove any spurious results from prior test runs
-for report in $(find -name surefire-reports); do
-    rm -rf $report
-done
-
-for i in $(seq 1 $rounds); do
-    echo "Round $i/$rounds"
+echo "" > rounds-test-results.csv
+for ((i=roundsStart;i<roundsStart+rounds;i++)); do
+    echo "Iteration: $((i-roundsStart+1)) / $rounds"
     export mvnTestRound=$i
-    /usr/bin/time -v /home/$SCRIPT_USERNAME/apache-maven/bin/mvn test -pl $module ${MVNOPTIONS} |& tee mvn-test-$i.log
-    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-	/usr/bin/time -v /home/$SCRIPT_USERNAME/apache-maven/bin/mvn test ${MVNOPTIONS} |& tee mvn-test-$i.log
-    fi
-    resDir="/Scratch/results-$modifiedslug-$module/$i"
-    mkdir $resDir
-    cp mvn-test-$i.log $resDir
-    for report in $(find -name surefire-reports); do
-	cp -r --parents $report $resDir
-	rm -rf $report
-    done
+
+    /usr/bin/time -v mvn test -pl $module ${MVNOPTIONS} |& tee mvn-test-$i.log
+
+    mkdir -p ${RESULTSDIR}/$i
+    mv mvn-test-$i.log ${RESULTSDIR}/$i
 done
-# # Run the plugin, random class first, method second
-# echo "*******************REED************************"
-# echo "Running testplugin for randomizemethods"
-# date
-
-# timeout ${timeout}s /home/$SCRIPT_USERNAME/apache-maven/bin/mvn testrunner:testplugin ${MVNOPTIONS} -Ddetector.timeout=${timeout} -Ddt.randomize.rounds=${rounds} -fn -B -e |& tee random_class_method.log
-
-
-# # Run the plugin, random class only
-# echo "*******************REED************************"
-# echo "Running testplugin for randomizeclasses"
-# date
-
-# timeout ${timeout}s /home/$SCRIPT_USERNAME/apache-maven/bin/mvn testrunner:testplugin ${MVNOPTIONS} -Ddetector.timeout=${timeout} -Ddt.randomize.rounds=${rounds} -Ddetector.detector_type=random-class -fn -B -e |& tee random_class.log
-
-# # Run the smart-shuffle (every test runs first and last)
-# echo "*******************REED************************"
-# echo "Running testplugin for smart-shuffle"
-# date
-
-# timeout ${timeout}s /home/$SCRIPT_USERNAME/apache-maven/bin/mvn testrunner:testplugin ${MVNOPTIONS} -Ddetector.timeout=${timeout} -Ddt.randomize.rounds=${rounds} -Ddetector.detector_type=smart-shuffle -fn -B -e |& tee smart_shuffle.log
-
-
-# Gather the results, put them up top
-# RESULTSDIR=/home/$SCRIPT_USERNAME/output/
-# mkdir -p ${RESULTSDIR}
-# /home/$SCRIPT_USERNAME/$TOOL_REPO/scripts/gather-results $(pwd) ${RESULTSDIR}
-# mv *.log ${RESULTSDIR}/
-
 
 echo "*******************REED************************"
 echo "Finished run_project.sh"
 date
 
-synchronization with docker host; see top of this file where existence of these pipes is checked for more details
-echo </dev/null >/Scratch/SCRIPTEND_${image}
-cat </Scratch/DATAREAD_${image} >/dev/null
+# synchronization with docker host; see top of this file where existence of these pipes is checked for more details
+echo </dev/null >/Scratch/SCRIPTEND_${runId}
+cat </Scratch/DATAREAD_${runId} >/dev/null
